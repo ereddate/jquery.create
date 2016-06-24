6825048dd5af7f0ec052f89a743910c9db0f5e50
yandong <ereddate@gmail.com>
2016-06-24 14:50:49 +0800

    Update jquery.create.js

--- jquery.create.js
+++ jquery.create.js
@@ -316,4 +316,188 @@ typeof jQuery != "undefined" && (function(win, $) {
 			return $;
 		}
 	};
-})(this, jQuery);
+
+	var rtrim = /^[\s\uFEFF\xA0]+|[\s\uFEFF\xA0]+$/g,
+		define;
+	win.define = define = function() {
+		var args = arguments,
+			len = args.length,
+			name, dependencies, factory;
+		if (len == 1) {
+			factory = args[0];
+		} else if (len == 2) {
+			name = args[0], factory = args[1];
+		} else if (len == 3) {
+			name = args[0], dependencies = args[1], factory = args[2];
+		}
+
+		_analyDefine((!name && ("model_" + Math.random()) || name), dependencies, factory);
+		return this;
+	};
+	define.amd = {};
+
+	function isArray(v) {
+		return typeof v != "undefined" && v.constructor == Array ? true : false;
+	};
+
+	function r(a) {
+		var b = "length" in a && a.length,
+			c = (typeof a).toLowerCase();
+		return "array" === c || 0 === b || "number" == typeof b && b > 0 && b - 1 in a
+	}
+
+	function each(a, b, c) {
+		var d, e = 0,
+			f = a.length,
+			g = r(a);
+		if (c) {
+			if (g) {
+				for (; f > e; e++)
+					if (d = b.apply(a[e], c), d === !1) break
+			} else
+				for (e in a)
+					if (d = b.apply(a[e], c), d === !1) break
+		} else if (g) {
+			for (; f > e; e++)
+				if (d = b.call(a[e], e, a[e]), d === !1) break
+		} else
+			for (e in a)
+				if (d = b.call(a[e], e, a[e]), d === !1) break;
+		return a
+	};
+
+	function trim(text) {
+		return text == null ?
+			"" :
+			(text + "").replace(rtrim, "");
+	}
+
+	function extend(target, args, filter) {
+		if (typeof args == "array" && !!args.length && args.length > 0) {
+			target = target || [];
+			var i, len;
+			for (i = 0; i < (len = args.length); i++) {
+				if (args[i] != filter)
+					target.push(args[i]);
+			}
+		} else {
+			target = target || {};
+			for (name in args) {
+				if (name != filter)
+					target[name] = args[name];
+			}
+		}
+		return target;
+	};
+
+	function isEmpty(v) {
+		var a = function(e) {
+			var t;
+			for (t in e) return !1;
+			return !0
+		};
+		return typeof v == "undefined" || v == null || typeof v == "string" && trim(v) == "" || isArray(v) && v.length == 0 || typeof v == "object" && a(v) ? true : false;
+	}
+
+	var model = function() {
+		return new model.fn.init();
+	};
+
+	model.fn = model.prototype = {
+		init: function() {
+			define.amd = {};
+			return this;
+		},
+		use: function(name, callback) {
+			var result;
+			if (isArray(name) || typeof name == "string" && name.split(' ').length > 1) {
+				result = {};
+				if (typeof name == "string") name = name.split(' ');
+				each(name, function(i, str) {
+					result[str] = _require(str);
+				});
+			} else {
+				result = _require(name);
+			}
+			callback && callback(result);
+			return this;
+		}
+	};
+
+	model.fn.init.prototype = model.fn;
+
+	define.require = model();
+
+	function _analyRequire(func) {
+		var funContext = func.toString(),
+			fixContext = trim(funContext.replace(/(\r|\n)/gi, ""));
+		var require = []
+		fixContext.replace(/\s*require\s*\(\s*[\"|\'](.+?)[\"|\']\s*\)\s*/gi, function(a, b) {
+			require.push(b);
+		});
+		return {
+			dependencies: require
+		};
+	}
+
+	function _require(name) {
+		var require = define.require,
+			options = define.amd[name] || false,
+			result;
+		if (name && options) {
+			if (options.status == 3) {
+				result = typeof options.exports == "function" && options.exports || options.exports;
+			} else if (options.status < 3 && options.status > 0) {
+				result = _exec(options);
+			}
+		}
+		return result;
+	}
+
+	function _exec(options) {
+		var _exprots = {},
+			module = {
+				exports: {}
+			},
+			than, result;
+		if (options.status == 2) {
+			each(options.dependencies, function(i, name) {
+				_require(name);
+			});
+		}
+		try {
+			result = options.factory(_require, _exprots, module),
+				than = result || _exprots || "exprots" in module && module.exports,
+				options.exports = than,
+				options.status = STATUS.executed;
+		} catch (e) {
+			options.exports = {},
+				options.status = STATUS.error;
+		}
+		return result;
+	}
+
+	var STATUS = {
+		loaded: 1,
+		readed: 2,
+		executed: 3,
+		error: 4
+	};
+
+	function _analyDefine(name, dependencies, factory) {
+		var options = {
+			name: (!name && ("model_" + Math.random()) || name),
+			dependencies: dependencies,
+			factory: factory,
+			exports: {},
+			status: isArray(dependencies) || typeof dependencies != "undefined" ? STATUS.readed : STATUS.loaded
+		};
+		var ops = _analyRequire(factory);
+		options = extend(options, ops);
+
+		define.amd[options.name] = options;
+
+		_exec(options);
+	}
+
+})(this, jQuery)
